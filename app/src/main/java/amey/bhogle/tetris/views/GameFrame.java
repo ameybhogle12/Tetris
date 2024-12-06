ad01e1b9977c69b5bd047cba720abb76486efdd0
ameybhogle12 <amey.bhogle12@gmail.com>
2024-12-06 13:15:46 +0530

    Initial Commit

---
+++ app/src/main/java/amey/bhogle/tetris/views/GameFrame.java
@@ -0,0 +1,25 @@
+package amey.bhogle.tetris.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class GameFrame extends View {
+    public GameFrame(Context context) {
+        super(context);
+    }
+
+    public GameFrame(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GameFrame(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public GameFrame(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
